b280609f5b9e949eab5f802cce8ab1fb5d473ceb
Fabio Arruda <fabio.arruda1@outlook.com>
2023-11-20 09:10:11 -0300

    nível do herói

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
-let firstName = "Fabio";
-let lastName = "Arruda";
+//Classificador de nível de Herói
+let nickName = "Thor";
+let xp = 0;
+let nivel = "";
 
-console.log("Meu nome é " + firstName + " " + lastName);
+if (xp <= 1000) {
+  nivel = " está no nível de Ferro";
+} else if (xp <= 2000) {
+  nivel = " está no nível de Bronze";
+} else if (xp <= 5000) {
+  nivel = " está no nível de Prata";
+} else if (xp <= 7000) {
+  nivel = " está no nível de Ouro";
+} else if (xp <= 8000) {
+  nivel = " está no nível de Platina";
+} else if (xp <= 9000) {
+  nivel = " está no nível Ascedente";
+} else if (xp <= 10000) {
+  nivel = " está no nível Imortal";
+} else if (xp >= 10000) {
+  nivel = " está no nível Radiante";
+}
+
+console.log("O Herói " + nickName + nivel);
